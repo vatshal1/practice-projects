a0c04e51ee4f442e190a5df663adf2a22e6fdc53
vatshal <vatshal.negi0727@gmail.com>
2024-09-25 22:26:50 +0530

    updation

--- projects using HTML,CSS & JS/mini-project using js/meme generator(fetch)/script.js
+++ projects using HTML,CSS & JS/mini-project using js/meme generator(fetch)/script.js
@@ -1,14 +1,19 @@
 const btn = document.querySelector(".meme-btn");
 const img = document.querySelector("img");
-let author = document.querySelector(".credit");
-let title = document.querySelector(".title");
+let author_name = document.querySelector(".credit");
+let title_name = document.querySelector(".title");
 
-btn.addEventListener("click", () => {
+btn.addEventListener("click", getMeme);
+
+function getMeme() {
   fetch("https://meme-api.com/gimme/wholesomememes")
     .then((res) => res.json())
     .then((data) => {
-      img.src = data.url;
-      title.innerText = data.title;
-      author.innerText = `meme author -> ${data.author}`;
+      const { author, title, url } = data;
+      img.src = url;
+      title_name.innerText = title;
+      author_name.innerText = `meme author -> ${author}`;
     });
-});
+}
+
+getMeme();
