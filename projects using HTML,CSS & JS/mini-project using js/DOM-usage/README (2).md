1a8a8bfe4da9d0f399a6c5208e74a9455369d3a3
vatshal <vatshal.negi0727@gmail.com>
2024-12-07 13:18:26 +0530

    changing file structures

--- projects using HTML,CSS & JS/mini-project using js/DOM-usage/README (2).md
+++
@@ -1,3 +0,0 @@
-
-# login-console-m-
-
--- projects using HTML,CSS & JS/mini-project using js/DOM-usage/script.js
+++
@@ -1,9 +0,0 @@
-frm = document.querySelector("form");
-frm.addEventListener("submit", function handleSubmit(e) {
-  e.preventDefault();
-  var username = document.getElementById("user");
-  var password = document.getElementById("pass");
-  console.log("Username:", username.value);
-  console.log("Password:", password.value);
-  console.log("submitted the details");
-});
